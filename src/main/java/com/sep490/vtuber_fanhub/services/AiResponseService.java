d47b0fc2c29125c9cea27ca18bbbca1188b1b59c
Minh Phàm Dân <phamdanminh@gmail.com>
2026-04-11 16:09:47 +0700

    no longer uses websocket, async messaging messages are now pagination removed websocket-related configs

--- src/main/java/com/sep490/vtuber_fanhub/services/AiResponseService.java
+++ src/main/java/com/sep490/vtuber_fanhub/services/AiResponseService.java
@@ -1,10 +1,9 @@
 package com.sep490.vtuber_fanhub.services;
 
-import com.sep490.vtuber_fanhub.dto.responses.AIMessageResponse;
+import com.sep490.vtuber_fanhub.dto.responses.MessageResponse;
 import com.sep490.vtuber_fanhub.models.User;
 
 public interface AiResponseService {
-    void generateAndSendReply(User user, String userMessageContent);
-    AIMessageResponse smartChat(String userPrompt, Long userId, Long sessionId);
+    MessageResponse generateAndSendReply(User user, String userMessageContent);
 
 }
