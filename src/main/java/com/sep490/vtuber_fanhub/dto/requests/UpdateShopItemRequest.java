ff7a9d322450bf12f27150986ccbcc33b7307ac6
Admin <khangdoan20122004@gmail.com>
2026-05-01 16:37:19 +0700

    fix

---
+++ src/main/java/com/sep490/vtuber_fanhub/dto/requests/UpdateShopItemRequest.java
@@ -0,0 +1,32 @@
+package com.sep490.vtuber_fanhub.dto.requests;
+
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
+import lombok.Getter;
+import lombok.Setter;
+import java.math.BigDecimal;
+
+@Getter
+@Setter
+public class UpdateShopItemRequest {
+
+    @NotBlank(message = "Item name must not be blank")
+    @Size(max = 100, message = "Item name must not exceed 100 characters")
+    private String itemName;
+
+    @Size(max = 1000, message = "Description must not exceed 1000 characters")
+    private String description;
+
+    @Size(max = 50, message = "Category must not exceed 50 characters")
+    private String category;
+
+    @NotNull(message = "Price must not be null")
+    private Long price;
+
+    private BigDecimal size;
+
+    private BigDecimal xAxis;
+
+    private BigDecimal yAxis;
+}
